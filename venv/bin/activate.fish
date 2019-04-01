71c57f0527e82febb87c3608c45ca4c921fb8b5a
DinoSaulo <sab2@cin.ufpe.br>
2019-04-01 15:49:52 -0300

    Sprint 0 - Projeto configurado para o deploy

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/saulo/Documentos/djangoecommerce-aula007/venv"
+set -gx VIRTUAL_ENV "/home/saulo/Documentos/git/Django-Ecommerce/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
